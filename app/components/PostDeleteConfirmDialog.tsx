e21b4adc7c9b4cf50aeab10382bee777c076302f
eliasc9 <ecamejo99@gmail.com>
2024-05-12 13:35:10 -0300

    deletion modal using headlessui

--- app/components/PostDeleteConfirmDialog.tsx
+++ app/components/PostDeleteConfirmDialog.tsx
@@ -1,23 +1,61 @@
 'use client'
 
+import { Button, Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
 import { useState } from "react";
 
 export default function PostDeleteConfirmDialog({ postId, children } : { postId?: number, children: React.ReactNode }) {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const onClick = (postId?: number) => {
-    setOpen(!open)
+  function open() {
+    setIsOpen(true)
+  }
+
+  function close() {
+    setIsOpen(false)
   }
 
   return (
     <>
-      <button className='' onClick={() => onClick(postId)}>
+      <button className='' onClick={open}>
         <span className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">Delete postId {postId}</span>
       </button>
 
-      <div className={open ? "" : "hidden"}>
-        {children}
-      </div>
+      <Transition appear show={isOpen}>
+        <Dialog as="div" className="relative z-10 focus:outline-none" onClose={close}>
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 transform-[scale(95%)]"
+                enterTo="opacity-100 transform-[scale(100%)]"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 transform-[scale(100%)]"
+                leaveTo="opacity-0 transform-[scale(95%)]"
+              >
+                <DialogPanel className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl">
+                  <DialogTitle as="h3" className="text-base/7 font-medium text-white">
+                    Confirm the deletion
+                  </DialogTitle>
+                  <p className="mt-2 text-sm/6 text-white/50">
+                    Are you sure?
+                  </p>
+                  <div className="mt-4">
+                    <Button
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      onClick={close}
+                    >
+                      Cancel
+                    </Button>
+                    <Button>
+                    {children}
+                    </Button>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   )
 }
