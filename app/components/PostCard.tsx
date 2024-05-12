5ea32dce710fe27c131cbbed415263fc66425e90
eliasc9 <ecamejo99@gmail.com>
2024-05-12 18:35:46 -0300

    QOL User id is a link

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -2,6 +2,7 @@ import PostDeleteConfirmDialog from '@/app/components/PostDeleteConfirmDialog'
 import PostDeleteButton from '@/app/components/PostDeleteButton'
 import ErrorBoundary from './ErrorBoundary'
 import Error from './Error'
+import Link from 'next/link'
 
 export type PostCardProps = {
   id: number,
@@ -20,7 +21,9 @@ export default async function PostCard({id, title, body, userId} : PostCardProps
         </div>
         <div className='flex flex-row'>
           <div>
+          <Link href={`/posts?userId=${userId}`}>
            <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">userId {userId}</span>
+          </Link>
           </div>
           
           <ErrorBoundary fallback={<Error title='An error has occurred' message='Please try again later.' />}>
